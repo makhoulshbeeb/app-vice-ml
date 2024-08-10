527b937dd54a0f9924627c5e396326e3ce40c7df
Abdallah <fafafavd@gmail.com>
2024-08-10 17:09:31 +0300

    fixed str type issue

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from app.model.model import mlflow_predict
 # from app.model.model import __version__ as model_version
 
 
 app = FastAPI()
+
+class TextIn(BaseModel):
+    text: str
 
 
 @app.get("/")
@@ -10,6 +15,6 @@ def home():
     return {"test": "ok"}
 
 
-# @app.post("/predict")
-# def predict(payload: TextIn):
-#     return {"language": mlflow_predict(payload.text)}
+@app.post("/predict")
+def predict(payload: TextIn):
+    return {"language": mlflow_predict(payload.text)}
